7ae735de5e9a6b652b259fd8e92b108718d2f544
LuisPa <LuisPa>
2017-06-19 16:01:17 -0600

    Adding Sweet Alert 2 on the site

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
 let button = new Vue({
     el:'#download-button',
+    data:{},
     methods:{
         showAlert: function(){
-            alert('Alerta!');
+            swal({
+            title: 'Auto close alert!',
+            text: 'I will close in 2 seconds.',
+            timer: 2000
+            }).then(
+            function () {},
+            // handling the promise rejection
+            function (dismiss) {
+                if (dismiss === 'timer') {
+                console.log('I was closed by the timer')
+                }
+            }
+            )
         }
     }
 })
